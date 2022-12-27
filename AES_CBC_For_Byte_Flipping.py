3e684989d6475b4c7618fcf2d2f5b21a7f98f0df
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-12-27 14:56:57 +0300

    Update AES_CBC_For_Byte_Flipping.py

--- AES_CBC_For_Byte_Flipping.py
+++ AES_CBC_For_Byte_Flipping.py
@@ -11,18 +11,18 @@ and algorith that can decipher the language used in AES Algorithm
 import requests
 import base64
 
-s=requests.Session()
+s = requests.Session()
 s.get("http://mercury.picoctf.net:25992/")
-cookie=s.cookies["auth_name"]
+cookie = s.cookies["auth_name"]
 print(cookie)
-unb64=base64.b64decode(cookie)
+unb64 = base64.b64decode(cookie)
 print(unb64)
-unb64b=base64.b64decode(unb64)
-for i in range (0,128):
-   pos=i//8
-   guessdec=unb64b[0:pos]+chr (ord(unb64b[pos])^(1<<(i%8)))+unb64b[pos+1:]
-   guessenc1 = base64.b64encode(guessdec)
-   guess=base64.b64encode(base64.b64encode(guessdec)).decode()
-   r=requests.get("http://mercury.picoctf.net:25992/",cookies={"auth_name": guess})
-   if "pico" in r.text:
-       print(r.text)
+unb64b = base64.b64decode(unb64)
+for i in range(0, 128):
+    pos = i // 8
+    guessdec = unb64b[0:pos] + chr(ord(unb64b[pos]) ^ (1 << (i % 8))) + unb64b[pos + 1:]
+    guessenc1 = base64.b64encode(guessdec)
+    guess = base64.b64encode(base64.b64encode(guessdec)).decode()
+    r = requests.get("http://mercury.picoctf.net:25992/", cookies={"auth_name": guess})
+    if "pico" in r.text:
+        print(r.text)

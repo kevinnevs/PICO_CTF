e60f71e76828f468fbfd5d6afb76c3b110abc03a
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-27 06:51:42 -0500

    initial commit

--- AES_CBC_For_Byte_Flipping.py
+++ AES_CBC_For_Byte_Flipping.py
@@ -8,12 +8,11 @@ Searching the cookie on the endpoint won't give a clue to find
 the flag. The only way to decrpyt the data is by actually finding
 and algorith that can decipher the language used in AES Algorithm
 """
-
 import requests
 import base64
 
 s=requests.Session()
-s.get("http://mercury.picoctf.net:56136/")
+s.get("http://mercury.picoctf.net:25992/")
 cookie=s.cookies["auth_name"]
 print(cookie)
 unb64=base64.b64decode(cookie)
@@ -21,9 +20,9 @@ print(unb64)
 unb64b=base64.b64decode(unb64)
 for i in range (0,128):
    pos=i//8
-   guessdec=unb64b[0:pos]+((unb64b[pos]^(1 &lt;&lt; (i%8))).to_bytes(1,'big'))+unb64b[pos+1:]
+   guessdec=unb64b[0:pos]+chr (ord(unb64b[pos])^(1<<(i%8)))+unb64b[pos+1:]
    guessenc1 = base64.b64encode(guessdec)
    guess=base64.b64encode(base64.b64encode(guessdec)).decode()
-   r=requests.get("http://mercury.picoctf.net:56136/",cookies={"auth_name": guess})
+   r=requests.get("http://mercury.picoctf.net:25992/",cookies={"auth_name": guess})
    if "pico" in r.text:
        print(r.text)

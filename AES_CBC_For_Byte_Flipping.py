9606f5bf78cad90106e210d8569b54fab7b391a5
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-12-27 14:24:56 +0300

    tracking changes

---
+++ AES_CBC_For_Byte_Flipping.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script written by Martin Carlisle on his YT channel :
+https://www.youtube.com/watch?v=Fs3EbH-Wdhc
+This script get's to Flip mode bit for a CTF challenge that 
+encrpyt's client side cookes using Hompophonic Encryption.
+Searching the cookie on the endpoint won't give a clue to find
+the flag. The only way to decrpyt the data is by actually finding
+and algorith that can decipher the language used in AES Algorithm
+"""
+
+import requests
+import base64
+
+s=requests.Session()
+s.get("http://mercury.picoctf.net:56136/")
+cookie=s.cookies["auth_name"]
+print(cookie)
+unb64=base64.b64decode(cookie)
+print(unb64)
+unb64b=base64.b64decode(unb64)
+for i in range (0,128):
+   pos=i//8
+   guessdec=unb64b[0:pos]+((unb64b[pos]^(1 &lt;&lt; (i%8))).to_bytes(1,'big'))+unb64b[pos+1:]
+   guessenc1 = base64.b64encode(guessdec)
+   guess=base64.b64encode(base64.b64encode(guessdec)).decode()
+   r=requests.get("http://mercury.picoctf.net:56136/",cookies={"auth_name": guess})
+   if "pico" in r.text:
+       print(r.text)

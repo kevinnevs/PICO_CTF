8aa4809a6d1bfc079cb02f654b01dde29717e258
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-12-28 09:32:12 +0300

    Update AES_CBC_For_Byte_Flipping.py

--- AES_CBC_For_Byte_Flipping.py
+++ AES_CBC_For_Byte_Flipping.py
@@ -1,28 +1,39 @@
-#!/usr/bin/python3
-"""
-Script written by Martin Carlisle on his YT channel :
-https://www.youtube.com/watch?v=Fs3EbH-Wdhc
-This script get's to Flip mode bit for a CTF challenge that 
-encrpyt's client side cookes using Hompophonic Encryption.
-Searching the cookie on the endpoint won't give a clue to find
-the flag. The only way to decrpyt the data is by actually finding
-and algorith that can decipher the language used in AES Algorithm
-"""
-import requests
-import base64
+# Script written by Mike on Tech on YT: /
+# https://youtu.be/i9KiOjeE-VY/
+# This script gets to decrypt by flipping the mode bit/
+# for a CTF challenge that ecnrpyt's client side cookies using Homomophonic/
+# Encryption. Searching the cookie on the endpoint won't give a/
+# to find the flag. The only way to decrypt the data is by actually/
+# finding alogrithm that can decipher the language used in AES/
+# Algorithm.
 
-s = requests.Session()
-s.get("http://mercury.picoctf.net:25992/")
-cookie = s.cookies["auth_name"]
-print(cookie)
-unb64 = base64.b64decode(cookie)
-print(unb64)
-unb64b = base64.b64decode(unb64)
-for i in range(0, 128):
-    pos = i // 8
-    guessdec = unb64b[0:pos] + chr(ord(unb64b[pos]) ^ (1 << (i % 8))) + unb64b[pos + 1:]
-    guessenc1 = base64.b64encode(guessdec)
-    guess = base64.b64encode(base64.b64encode(guessdec)).decode()
-    r = requests.get("http://mercury.picoctf.net:25992/", cookies={"auth_name": guess})
-    if "pico" in r.text:
-        print(r.text)
+from base64 import b64decode
+from base64 import b64encode
+import requests
+
+#https://play.picoctf.org/practice/challenge/124?category=1&page=1&search=
+
+original_cookie = b64decode("UHJQRkdWTyswVFhhMHFrRktmNnQ3WmhuWUtVc09sTHB4VFgzSllIQUJYRXNBWkY4Q1RLMmVGZENCVld6MVhhbS8xWk5vKzBIQmd0SmVaRzJzUklkTE81dFFOb1FBMnNjMk1kK3VDeXEzUVk1K3JHTnYzMmVlNHRyVmsvdllITDI=")
+original_cookie = bytearray(original_cookie)
+
+def bitFlip(cookie_char_pos:int, bit_pos:int) -> str:
+    altered_cookie = bytearray(original_cookie)
+
+    flipped = altered_cookie[cookie_char_pos]^bit_pos
+
+    altered_cookie[cookie_char_pos] = flipped
+
+    altered_cookie_b64 = b64encode(bytes(altered_cookie))
+
+    return altered_cookie_b64.decode("utf-8")
+
+for cookie_char_pos in range(len(original_cookie)):
+  print(f"Checking cookie position: {cookie_char_pos} ")
+  for bit_pos in range(128): # [1,2,4,8,16,32,64,128]: #byte stream - 8 bit range affords 128 possiblities
+    altered_cookie = bitFlip(cookie_char_pos, bit_pos)
+    cookies = {'auth_name': altered_cookie}
+    r = requests.get('http://mercury.picoctf.net:21553/', cookies=cookies)
+    t = r.text.lower()
+    if "picoCTF{".lower() in t or "picoCTF {".lower() in t:
+      print(r.text)
+      break
